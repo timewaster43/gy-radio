b7d15eb87c173d1ed0ee4c7e11281ab5ebb5cfaf
Benjamin Li <3486283808@qq.com>
2023-11-02 14:37:48 +0800

    Update forms.py
    add more forms

--- forms.py
+++ forms.py
@@ -6,3 +6,16 @@ class LoginForm(FlaskForm):
     username = StringField(label='用户名', default='admin')
     password = PasswordField(label='密码')
     submit = SubmitField('登录')
+
+class register(FlaskForm):
+    """ 登录表单的实现 """
+    uid = StringField(label="学号")
+    username = StringField(label='姓名', default='xxx')
+    password = PasswordField(label='密码')
+    submit = SubmitField('注册')
+
+class orderForm(FlaskForm):
+    """ 登录表单的实现 """
+    song_name = StringField(label="歌名")
+    song_artist = StringField(label='歌手')
+    submit = SubmitField('提交')
