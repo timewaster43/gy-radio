4946836d9d79eb783fa54f6ecbfb38484f4373c0
Benjamin Li <3486283808@qq.com>
2024-01-09 09:14:12 +0800

    Update config.py
    add more protected urls, set different auth id, give a formula to work out working day

--- config.py
+++ config.py
@@ -1,4 +1,25 @@
 required_login = [
     "/dashboard", 
-    "/song_choice", 
+    "/song_choice",
+    "/admin_songs",
+    "/arrange",
 ]
+
+auth = ["站长", "节目副站", "线上节目总监", "线下节目总监", "主持副站", "主持总监", "宣传副站", "宣传总监", "声带副站", "声带总监", "技术副站", "技术总监", "高干", "站员"]
+
+def program_id(id):
+    week = ""
+    day = ""
+    period = ""
+    if (id//100)==1:
+        week = "单周"
+    else:
+        week = "双周"
+    day_list = ["一", "二", "三", "四", "五"]
+    day = day_list[((id%100)//10)-1]
+    if id%10 == 1:
+        period = "上午"
+    else:
+        period = "下午"
+
+    return week+"周"+day+period
